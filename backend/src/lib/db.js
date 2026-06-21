8df4acdf00663da7ca8106b342c29585d73624ca
Tommy Ju <tommyju25@gmail.com>
2026-06-21 10:58:22 -0700

    Setup backend to use DB connection string in production

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,21 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: isProduction ? { rejectUnauthorized: false } : false,
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+      }
+);
 
 export default pool;
